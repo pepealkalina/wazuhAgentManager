fc92d51617c3088fa84fb4ea1175a3684d351567
pepealkalina <111188050+pepealkalina@users.noreply.github.com>
2025-05-06 12:08:54 +0200

    manageWazuhAgent.sh

---
+++ manageWazuhAgent.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+# Creator: Pedro Reina
+# Date: 2025-05-06
+# Description: Automates the process of deploying wazuh agents in linux depending on architecture and package system for Wazuh version 4.11.2
+
+# WARNING: IF THE VERSION OF THE SERVER IS NOT 4.11.2 PLEASE CHANGE THE 'wazuhServerVersion' VARIABLE
+
+### Wazuh Server Version ###
+# Set the wazuh server version to get the package
+wazuhServerVersion="4.11.2"
+###
+
+### Wazuh Agent Deploy Info ###
+# Set the Wazuh info for deploy the package
+unset wazuhManager
+unset wazuhAgentGroup
+unset wazuhAgentName
+###
+
+
+
+function installPackageDPKG()
+{
+    # Get the server root version from the wazuhServerVersion, if it is 4.11.2 root version is 4, or is 3.5 root version is 3
+    rootVersion=${wazuhServerVersion:0:1}
+
+    #system Architecture
+    systemArch=$1
+    
+    echo "Getting Wazuh Agent package..."
+    wget -q "https://packages.wazuh.com/$rootVersion.x/apt/pool/main/w/wazuh-agent/wazuh-agent_$wazuhServerVersion-1_$systemArch.deb" 2>/dev/null
+
+    if [ $? -eq 0 ] && [ -e "./wazuh-agent_$wazuhServerVersion-1_$systemArch.deb" ]; then
+        echo "Installing Wazuh Agent package..."
+        echo "Set the server address, enter an IP address or a FQDN"
+        while [ -z $wazuhManager  ]; do
+            read -p "-> " wazuhManager
+        done
+        echo "Set Agent's Name, "
+            read -p "-> " wazuhAgentGroup
+        echo "Set Agent's group/s, is there more than one group use <group1,group2,...> format"
+        read -p "-> " wazuhAgentGroup
+        sudo WAZUH_MANAGER=$wazuhManager WAZUH_AGENT_GROUP="$wazuhAgentGroup" WAZUH_AGENT_NAME="$wazuhAgentName" dpkg -i "./wazuh-agent_$wazuhServerVersion-1_$systemArch.deb"
+        if [ $? -ne 0 ]; then
+            echo "ERROR: Could not install the package, check the package name"
+        fi
+    else
+        echo "ERROR: Could not get the package, check the url and versions please"
+    fi
+}
+
+function installPackageRPM()
+{
+    # Get the server root version from the wazuhServerVersion, if it is 4.11.2 root version is 4, or is 3.5 root version is 3
+    rootVersion=${wazuhServerVersion:0:1}
+
+    #system Architecture
+    systemArch=$1
+    
+    echo "Getting Wazuh Agent package..."
+    curl -o wazuh-agent-4.11.2-1.x86_64.rpm https://packages.wazuh.com/$rootVersion.x/yum/wazuh-agent-$wazuhServerVersion-1.$systemArch.rpm  2>/dev/null
+
+    if [ $? -eq 0 ] && [ -e "./wazuh-agent_$wazuhServerVersion-1_$systemArch.deb" ]; then
+        echo "Installing Wazuh Agent package..."
+        echo "Set the server address, the agent use it to communicate with the server, enter an IP address or a fully qualified domain name (FQDN)"
+        while [ -z $wazuhManager  ]; do
+            read -p "-> " wazuhManager
+        done
+        #sudo WAZUH_MANAGER=$wazuhManager WAZUH_AGENT_GROUP=$wazuhAgentGroup WAZUH_AGENT_NAME=$wazuhAgentName rpm -ihv wazuh-agent-4.11.2-1.x86_64.rpm
+        if [ $? -ne 0 ]; then
+            echo "ERROR: Could not install the package, check the package name"
+        fi
+    else
+        echo "ERROR: Could not get the package, check the url and versions please"
+    fi
+}
+
+function installWazuhAgent()
+{   
+    if dpkg -S ls >/dev/null 2>&1
+        then
+            case "$(uname -m)" in
+                x86_64)
+                    installPackageDPKG "amd64";;
+                amd64)
+                    installPackageDPKG "amd64";;
+                aarch64)
+                    installPackageDPKG "arm64";;
+                *)
+                    echo "ERROR: Don't know how to handle $(uname -m)"
+                    exit 1
+                ;;
+            esac
+    elif rpm -q -f /bin/ls >/dev/null 2>&1
+        then
+            case "$(uname -m)" in
+                x86_64)
+                    installPackageRPM "x86_64";;
+                amd64)
+                    installPackageRPM "x86_64";;
+                aarch64)
+                    installPackageRPM "aarch64";;
+                *)
+                    echo "Don't know how to handle $(uname -m)"
+                    exit 1
+                ;;
+            esac
+    else
+        echo "Don't know this package system (neither RPM nor DEB)."
+        exit 1
+    fi
+}
+
+function startWazuhAgentService()
+{
+    sudo systemctl daemon-reload
+    sudo systemctl enable wazuh-agent
+    sudo systemctl start wazuh-agent
+}
+
+function showHelp()
+{
+    echo -e "\
+This is a usage to explain how $1 is executed\n\n\
+\t$1 install -> Get the Wazuh-Agent package and install it with the parameters readed from stdin"
+}
+
+function main()
+{
+    if [ -n "$SUDO_USER" ] || [ "$USER" = "root" ]; then
+        case $1 in
+            install)
+                installWazuhAgent;;
+            uninstall)
+                sudo dpkg --purge wazuh-agent;;
+            start)
+                startWazuhAgentService;;
+            stop)
+                sudo systemctl stop wazuh-agent;;
+            restart)
+                sudo systemctl restart wazuh-agent;;
+            status)
+                sudo systemctl status wazuh-agent;;
+            *)
+                echo "ERROR: Not a valid option execute $0 help to show a usage"
+            ;;
+        esac
+    elif [ "$1" = "help" ]; then
+        showHelp $0
+    else
+        echo "ERROR: This script need sudo or root privileges to execute"
+        exit 1
+    fi
+}
+
+if [ $# -eq 1 ]; then
+    main $1
+else
+    echo "ERROR: The scrip need a valid option execute $0 help to show a usage"
+fi
